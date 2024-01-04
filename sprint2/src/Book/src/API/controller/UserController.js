8215290b61437ed805607efb0d89adfb1b80584c
N <blueshark.1949@gmai.com>
2024-01-04 09:28:10 +0900

    유저 API

---
+++ sprint2/src/Book/src/API/controller/UserController.js
@@ -0,0 +1,107 @@
+const conn = require("../mariadb");
+const { StatusCodes } = require("http-status-codes");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const join = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = "insert into users (email, password, salt) values (?, ?, ?)";
+
+  const salt = crypto.randomBytes(10).toString("base64");
+  const hashPassword = crypto
+    .pbkdf2Sync(password, salt, 10000, 10, 64, "sha512")
+    .toString("base64");
+
+  let values = [email, hashPassword, salt];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    return res.status(StatusCodes.CREATED).json(results);
+  });
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+  let sql = "select * from users where email = ?";
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    const loginUser = results[0];
+
+    const hashPassword = crypto
+      .pbkdf2Sync(password, loginUser.salt, 10000, 10, 64, "sha512")
+      .toString("base64");
+
+    if (loginUser && loginUser.password == hashPassword) {
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: "5m",
+          issuer: "N",
+        }
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+      console.log(token);
+
+      return res.status(StatusCodes.OK).join(results);
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
+};
+
+const passordResetRequest = (req, res) => {
+  const { email } = req.body;
+  let sql = "select * from users where email = ?";
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    const user = results[0];
+    if (user) {
+      return res.status(StatusCodes.OK).end();
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
+};
+
+const passwordReset = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = "update users set password=?, salt=? where email=?";
+  const salt = crypto.randomBytes(10).toString("base64");
+  const hashPassword = crypto
+    .pbkdf2Sync(password, salt, 10000, 10, 64, "sha512")
+    .toString("base64");
+
+  let values = [hashPassword, salt, email];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.affectedRows == 0) {
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    } else {
+      return res.status(StatusCodes.OK).json(results);
+    }
+  });
+};
+
+module.exports = { join, login, passordResetRequest, passwordReset };
---
+++ sprint2/src/Book/src/API/routes/users.js
@@ -0,0 +1,19 @@
+// 기본 설정
+const express = require("express");
+const router = express.Router();
+const conn = require("../mariadb");
+const {
+  join,
+  login,
+  passordResetRequest,
+  passwordReset,
+} = require("../controller/UserController");
+
+router.use(express.json());
+
+router.post("/join", join);
+router.post("/login", login);
+router.post("/reset", passordResetRequest);
+router.put("/reset", passwordReset);
+
+module.exports = router;
