a4f135b6955312f0a166e8c203668ae187d6fd3f
N <blueshark.1949@gmai.com>
2024-01-12 11:19:23 +0900

    검색 기능 추가

---
+++ pari-coding/add-in/SearchController.js
@@ -0,0 +1,29 @@
+const conn = require("../mariadb");
+const { StatusCodes } = require("http-status-codes");
+
+const allSearch = (req, res) => {
+    const { word } = req.query;
+
+    if (!word) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ error: "검색어를 입력하세요." });
+    }
+
+    try {
+        let sql = `select * from books where title LIKE ? OR summary LIKE ? OR detail LIKE ? OR author LIKE ?`;
+        const values = [word, word, word, word];
+        conn.query(sql, values, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.NOT_FOUND).end();
+            }
+            if (results) {
+                return res.status(StatusCodes.OK).json(results);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "검색에 실패했습니다." });
+    }
+};
+
+module.exports = { allSearch };
